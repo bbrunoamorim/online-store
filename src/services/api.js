492364d7f5a7f10f419a6951fb3c2e4895325e2e
Bruno Amorim <bbrunoamorim2311@gmail.com>
2022-09-08 18:56:57 -0300

    Update: Requirement 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,29 @@
 export async function getCategories() {
   const END_POINT = 'https://api.mercadolibre.com/sites/MLB/categories';
-  const response = await fetch(END_POINT);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(END_POINT);
+    return response.json();
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  if (query) {
-    const QUERY_END_POINT = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-    const queryResponse = await fetch(QUERY_END_POINT);
-    const queryData = await queryResponse.json();
-    return queryData;
+  const END_POINT = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  try {
+    const response = await fetch(END_POINT);
+    return response.json();
+  } catch (error) {
+    return error;
   }
-  const CATEGORY_END_POINT = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-  const categoryResponse = await fetch(CATEGORY_END_POINT);
-  const categoryData = await categoryResponse.json();
-  return categoryData;
 }
 
 export async function getProductById(productId) {
   const END_POINT = `https://api.mercadolibre.com/items/${productId}`;
-  const response = await fetch(END_POINT);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(END_POINT);
+    return response.json();
+  } catch (error) {
+    return error;
+  }
 }
