083d928dc7ee5d5af488ffdbb938057a038aa210
Mikael-Vinicius <mikaelvinimarques@hotmail.com>
2022-09-08 20:14:38 -0300

    requisito 4

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import { getCategories } from '../services/api';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
+    this.ApiCaller = this.ApiCaller.bind(this);
 
     this.state = {
       list: [],
+      categories: '',
     };
   }
 
+  componentDidMount() {
+    this.ApiCaller();
+  }
+
+  ApiCaller = () => {
+    getCategories().then((data) => this.setState({ categories: data }));
+  };
+
   render() {
-    const { list } = this.state;
+    this.ApiCaller();
+    const { list, categories } = this.state;
+    console.log(categories);
     const hasItemOnList = list.length > 0;
     return (
       <>
@@ -23,6 +36,15 @@ export default class Main extends Component {
               Digite algum termo de pesquisa ou escolha uma categoria.
             </p>
           )}
+        </div>
+        <div>
+          { categories !== ''
+        && categories.map((categoria) => (<input
+          data-testid="category"
+          value={ categoria.name }
+          type="button"
+          key={ categoria.id }
+        />))}
         </div>
       </>
     );
