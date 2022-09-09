e8758d013ad3c83c8de4623e6d96551651acb214
Bruno Amorim <bbrunoamorim2311@gmail.com>
2022-09-08 21:51:15 -0300

    Update: requirement 05

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main';
 import ProductsResult from './components/ProductsResult';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Route exact path="/">
           <Main />
           <ProductsResult />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
         </Route>
       </Switch>
     </BrowserRouter>
